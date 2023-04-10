af0acf1142d63b52d720fad7bcc8dcad15d6c227
andychen <strikecbu@gmail.com>
2023-04-10 22:10:43 +0800

    init

---
+++ moneywiz-parser/src/test/java/com/andy/moneywizparser/MoneywizParserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andy.moneywizparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneywizParserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
